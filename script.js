296da0b8c97a977e47e93a4018af79fd0f1aef29
schmilco <karenl483@hotmail.com>
2019-04-12 14:35:38 +1200

    js removed for designer headings/filter

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-var header = document.getElementById("flex-container-designers");
-var btns = header.getElementsByClassName("designer-name");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
-  });
-}
-
-// Get the modal
-// var modal = document.getElementById('myModal');
-
-// // Get the image and insert it inside the modal - use its "alt" text as a caption
-// var img = document.getElementById('content-image');
-// var modalImg = document.getElementById("img01");
-// var captionText = document.getElementById("caption");
-// img.onclick = function(){
-//   modal.style.display = "block";
-//   modalImg.src = this.src;
-//   captionText.innerHTML = this.alt;
-// }
-
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-
-// // When the user clicks on <span> (x), close the modal
-// // span.onclick = function() { 
-// //   modal.style.display = "none";
-// // }
-
 var key = 'SCJnOBwjJqgpwxIybOHvs0cUt0XRrydH';	// Your unique key - https://www.behance.net/dev
 var behanceUser = 'hello6e2d'; 				// example - Manuel from Yoobee Creative Catchup #3 | ellastoner370c
 
