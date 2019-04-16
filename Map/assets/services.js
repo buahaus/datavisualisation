0d2441f10f6999cedbaa11598d4daf460a1e4b3e
schmilco <karenl483@hotmail.com>
2019-04-17 09:17:09 +1200

    map file added

---
+++ Map/assets/services.js
@@ -0,0 +1,10 @@
+// SERVICE MARKERS DATA (example how we can create multiple markers storing data in OBJECTS)
+var services = [
+    {
+      name: 'Service 01',
+      latlng: [ -36.880906, 174.786343],
+      description: 'Design Distillery',
+      iconImage: 'assets/logo.svg'
+    },
+
+];
